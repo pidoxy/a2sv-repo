8ce2ea2775119509d486f1160a1dea5b5c4531ff
IDOKO EMMANUEL <eemmanuel.idoko@gmail.com>
2026-01-28 20:01:47 +0100

    Create listComprehension.py

---
+++ listComprehension.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+result = []
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            total = i + j + k
+            if total != n:
+                result.append([i,j,k])
+
+print(result)
+                
