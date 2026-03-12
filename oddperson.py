91f9bd14400a060fc7040c2e5e7e9f6c46a66f5b
IDOKO EMMANUEL <eemmanuel.idoko@gmail.com>
2026-03-12 21:05:33 +0100

    update

---
+++ oddperson.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+a = list(map(int, input().split()))
+
+even = False
+odd = False
+
+for x in a:
+    if x % 2 == 0:
+        even = True
+    else:
+        odd = True
+
+    if even and odd:
+        break
+
+if even and odd:
+    a.sort()
+
+print(*(a))
